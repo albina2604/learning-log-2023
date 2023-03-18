403706bb07887b113e7f4e93c412525296c3424a
albina2604 <albinamilshyna2604@gmail.com>
2023-03-18 13:56:49 +0200

    Add edit entry page

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -16,5 +16,7 @@ urlpatterns = [
     path('new_topic/', views.new_topic, name='new_topic'),
     # Сторінка для додавання нового запису
     path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # Сторінка для редагування запису
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 # Create your views here.
@@ -58,3 +58,24 @@ def new_entry(request, topic_id):
 
     context = {'form': form, 'topic': topic, }
     return render(request, 'learning_logs/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """Редагує існуючий запис"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+
+    context = {
+        'entry': entry,
+        'topic': topic,
+        'form': form,
+    }
+    return render(request, 'learning_logs/edit_entry.html', context)
