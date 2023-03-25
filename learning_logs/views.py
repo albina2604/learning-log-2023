7de2b33b34076627ee40f09ac7a09574df7f79c5
albina2604 <albinamilshyna2604@gmail.com>
2023-03-25 13:41:33 +0200

    Add login_required for topics page

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -12,6 +13,7 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 
+@login_required
 def topics(request):
     """Виводить список тем"""
     topics = Topic.objects.order_by('date_added')
